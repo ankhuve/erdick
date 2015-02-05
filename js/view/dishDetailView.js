8d152b79f55092600cfd75ac02b7d359c3ad13d0
ankhuve <erik.c.forsberg@gmail.com>
2015-02-05 13:21:14 +0100

    Added buttons

--- js/view/dishDetailView.js
+++ js/view/dishDetailView.js
@@ -2,15 +2,16 @@
 var DishDetailView = function (container, model) {
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
+ 	var dishID = 1;
 	this.dishName = container.find("#nameOfDish");
-	this.dishName.html(model.getDish(1).name);
+	this.dishName.html(model.getDish(dishID).name);
 	this.menuDetails = container.find("#dishOverview");
-	this.menuDetails.html(model.getDishOverview(1));
+	this.menuDetails.html(model.getDishOverview(dishID));
 
 	this.dishPic = container.find("#dishImg");
-	this.dishPic.html("<img src='images/"+model.getDish(1).image+"' width='300px' height='300px'>");
+	this.dishPic.html("<img src='images/"+model.getDish(dishID).image+"' width='350px' class='img-thumbnail'>");
 
 	this.dishDescription = container.find("#dishDesc");
-	this.dishDescription.html(model.getDish(1).description);
+	this.dishDescription.html(model.getDish(dishID).description);
 
 }
